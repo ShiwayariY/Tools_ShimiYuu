8e21578d7db048f98087b754e57000836cf94c49
Shiwayari <shiwayari@gmail.com>
2020-08-04 06:27:53 +0200

    Add interleave function

--- include/helper.hh
+++ include/helper.hh
@@ -1,5 +1,6 @@
 #include <string>
 #include <string_view>
+#include <iterator>
 
 #ifndef INCLUDE_SHIMIYUU_HELPER_HH_
 #define INCLUDE_SHIMIYUU_HELPER_HH_
@@ -7,6 +8,24 @@
 namespace shimiyuu::helper {
 
 std::string sanitize_windows_filename(const std::string& filename);
+
+template<typename IterT>
+std::string interleave(IterT begin, IterT end, std::string delim = ", ",
+		std::string (*to_string)(const typename std::iterator_traits<IterT>::value_type&)
+		= [](const typename std::iterator_traits<IterT>::value_type& token) {
+			return token;
+		}, std::string token_prefix = "", std::string token_suffix = "") {
+	std::string ret;
+
+	bool print_delim = false;
+	while (begin != end) {
+		if (print_delim) ret += delim;
+		ret += token_prefix + to_string(*begin++) + token_suffix;
+		print_delim = true;
+	}
+
+	return ret;
+}
 
 }
 
