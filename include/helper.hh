b4b733e87a6f934b9c503f0ff889437727c981c9
Shiwayari <shiwayari@gmail.com>
2020-08-22 00:32:26 +0200

    Add functions apply_permutation, sort_according_to

--- include/helper.hh
+++ include/helper.hh
@@ -2,6 +2,7 @@
 #include <iterator>
 #include <sstream>
 #include <filesystem>
+#include <numeric>
 
 #ifndef INCLUDE_SHIMIYUU_HELPER_HH_
 #define INCLUDE_SHIMIYUU_HELPER_HH_
@@ -36,6 +37,42 @@ std::string interleave(IterT begin, IterT end, std::string delim = ", ",
 	}
 
 	return ret;
+}
+
+template<typename Container, typename ... Containers>
+void apply_permutation(const std::vector<std::size_t>& permutation,
+		Container& to_sort, Containers& ... rest) {
+
+	std::vector<bool> done(to_sort.size());
+	for (size_t i = 0; i < to_sort.size(); ++i) {
+		if (done[i]) continue;
+		done[i] = true;
+		std::size_t prev = i;
+		std::size_t curr = permutation[i];
+		while (i != curr) {
+			std::swap(*std::next(to_sort.begin(), prev), *std::next(to_sort.begin(), curr));
+//			std::swap(to_sort[prev], to_sort[curr]);
+			(std::swap(*std::next(rest.begin(), prev), *std::next(rest.begin(), curr)), ...);
+//			(std::swap(rest[prev], rest[curr]), ...);
+			done[curr] = true;
+			prev = curr;
+			curr = permutation[curr];
+		}
+	}
+}
+
+template<typename Container, typename Compare, typename ... Containers>
+void sort_according_to(const Container& reference, Compare compare, Containers& ... to_sort) {
+
+	std::vector<std::size_t> sort_permutation(reference.size());
+	std::iota(sort_permutation.begin(), sort_permutation.end(), 0);
+	std::sort(sort_permutation.begin(), sort_permutation.end(),
+			[&](std::size_t i, std::size_t j) {
+				return compare(
+						*std::next(reference.begin(), i),
+						*std::next(reference.begin(), j));
+			});
+	apply_permutation(sort_permutation, to_sort ...);
 }
 
 }
